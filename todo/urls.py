9e647429f5f30ebab99136877ead0f0cb16e6f1e
Joomart1227 <satylkanovvv@gmail.com>
2023-03-16 20:00:13 +0600

    comit

--- todo/urls.py
+++ todo/urls.py
@@ -14,11 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from todo_app.views import get_todos, create_todo
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My ToDo API",
+      default_version='v1',
+      description="API napisannyi na django",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', get_todos),
-    path('create-todo/', create_todo),
+    path('api/v1/', include('todo_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,9 +2,12 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import ToDo
-from .serializers import ToDoSerializer
+from .serializers import ToDoSerializer, UpdateToDoSerializer
 
 
 @api_view(['GET'])
@@ -16,7 +19,7 @@ def get_todos(request: Request):
     print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+@swagger_auto_schema(method='POST', request_bady=ToDoSerializer)
 @api_view(['POST'])
 def create_todo(request: Request) -> Response:
     serializer = ToDoSerializer(data=request.data)
@@ -29,3 +32,34 @@ def create_todo(request: Request) -> Response:
     #     todo.save()
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(method='PATCH', request_body=UpdateToDoSerializer)
+@api_view(['PATCH'])
+def update_todo(request: Request, pk) -> Response:
+    print(pk)
+    try:
+        todo = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response({'detail': f'ToDo with {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    serializer = UpdateToDoSerializer(
+        instance=todo,
+        data=request.data
+        )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def delete_todo(request: Request, pk) -> Response:
+    try:
+        todo  = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response({'detail': f'ToDo with {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    todo.delete()
+    return Response({'details': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def get_todo(request: Request, pk) -> Response:
+    todo = get_object_or_404(ToDo, pk=pk)
+    serializer = ToDoSerializer(instance=todo)
+    return Response(serializer.data, status=status.HTTP_200_OK)
